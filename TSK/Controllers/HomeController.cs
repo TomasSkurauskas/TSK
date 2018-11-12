525d5c80ea3e749ab6233eaa026a33fa08c79b63
Tomas Skurauskas <skurauskas@hotmail.com>
2018-11-12 23:31:22 +0000

    Kick Start

--- TSK/Controllers/HomeController.cs
+++ TSK/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TSK.Data;
 
 namespace TSK.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITSKDbContext _context;
+
+        public HomeController()
+        {
+            _context = new MyDbContext();
+        }
+        [Authorize]
+        public ActionResult Test()
+        {
+            var test = _context.AspNetUsers.ToList();
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
